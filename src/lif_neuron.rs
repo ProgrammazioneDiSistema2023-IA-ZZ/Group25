5abceffb2f5f00ce5d92d34b7685928eb717b45e
s268588@studenti.polito.it <s268588@studenti.polito.it>
2024-01-04 17:46:03 +0100

    neurone creato

--- src/lif_neuron.rs
+++ src/lif_neuron.rs
@@ -1,35 +1,40 @@
 // lif_neuron.rs
 
-pub struct SpikingNeuron<T> {
+pub trait Neuron: 'static + Debug + Clone {
+    type ClassNeuron: 'static + Sized + Clone + Sync + RefInto<Self::SolverVars>;}
+
+pub struct LIFNeuron {
     membrane_potential: f64,
     reset_potential: f64,
     resting_potential: f64,
     threshold: f64,
-    decay_factor: f64,
-    synaptic_weights: Vec<T>,
 }
 
-impl<T> SpikingNeuron<T> {
+impl LIFNeuron {
     pub fn new(
         reset_potential: f64,
         resting_potential: f64,
-        threshold: f64,
-        decay_factor: f64,
-        synaptic_weights: Vec<T>,
+        threshold: f64
     ) -> Self {
         Self {
             membrane_potential: resting_potential,
             reset_potential,
             resting_potential,
-            threshold,
-            decay_factor,
-            synaptic_weights,
+            threshold
         }
     }
 
+#[derive(Clone, Copy, Debug)]
+pub struct LeakyIntegrateFire;
+
+impl Neuron for LeakyIntegrateFire {
+    type ClassNeuron = LIFNeuron;}
+    
     pub fn integrate(&mut self, input_spikes: &[T], time_step: f64) -> bool {
         // Logica di integrazione qui
         // Aggiorna il potenziale di membrana in base agli input e al tempo
+
+        //cambiare synaptic_weights, perchè sara una matrice fuori
         let weighted_sum: f64 = input_spikes.iter().zip(&self.synaptic_weights).map(|(x, w)| *x * *w).sum();
         self.membrane_potential = self.membrane_potential * self.decay_factor + weighted_sum;
 
